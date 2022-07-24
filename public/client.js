53c65d2c3bf6fc3d3421090bee246312b5691954
muhamad.zolfaghari <you@example.com>
2022-07-24 22:16:03 +0430

    The client.js and server.js were update due to finalisation.

--- public/client.js
+++ public/client.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+  $("form").submit(function () {
+    var messageToSend = $("#m").val();
+    let socket = io();
 
-    $('#m').val('');
+    socket.on("user count", function (data) {
+      console.log(data);
+    });
+
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
 });
--- server.js
+++ server.js
@@ -15,19 +15,29 @@ const auth = require("./auth");
 
 const app = express();
 const http = require("http").createServer(app);
-const socketIo = require('socket.io');
-const io = require('socket.io')(http);
-
-io.on("connection", (socket) => {
-  console.log("A user has connected");
-});
-
+const io = require("socket.io")(http);
 fccTesting(app); //For FCC testing purposes
+
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 // app.set('views/pug', path.join(__dirname, 'views', 'pug'))
+
+const MongoStore = require("connect-mongo")(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+  })
+);
 
 app.use(
   session({
@@ -52,6 +62,17 @@ myDB(async (client) => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let socket = io();
+  io.connect("http://localhost/auth/github/callback");
+  io.on("connection", (socket) => {
+    console.log("A user has connected");
+    let currentUsers = 0;
+    ++currentUsers;
+    io.emit("user count", currentUsers);
+  });
+  socket.on("disconnect", () => {
+    /*anything you want to do on disconnect*/
+  });
   // Be sure to add this...
 }).catch((e) => {
   app.route("/").get((req, res) => {
